348c2a46c1f35d7bfb3d4e49d60c4eaf3d9716f9
plunched <smoll.pp.exe@gmail.com>
2021-04-22 20:36:15 +0200

    serverinfo update

--- src/commands/information/serverInfo.ts
+++ src/commands/information/serverInfo.ts
@@ -1,5 +1,7 @@
 import { Command } from "discord-akairo";
 import { Message, MessageEmbed } from "discord.js";
+import { formatDistanceToNow } from "date-fns";
+import { pool } from "../../db";
 const ms = require("ms");
 const online = "<:online:830091620845224026>";
 const offline = "<:offline:830091284483670046>";
@@ -29,6 +31,11 @@ export default class botInviteCommand extends Command {
     const voiceChannels = message.guild.channels.cache.filter(
       (channel) => channel.type === "voice"
     ).size;
+
+    const totalCommands = await pool.query(
+      "SELECT totalCommands FROM guilds WHERE guildID = $1",
+      [message.guild.id]
+    );
     message.util.send(
       new MessageEmbed()
         .setColor("738adb")
@@ -46,29 +53,29 @@ export default class botInviteCommand extends Command {
           },
           {
             name: "members",
-            value: `${online} ${onlineMembers} online\n${offline} ${message.guild.memberCount} offline`,
-            inline: true,
+            value: `${online} ${onlineMembers} online  |  ${offline} ${message.guild.memberCount} offline`,
+            inline: false,
           },
           {
             name: "channels",
-            value: `# ${textChannels} text\n${voiceChannels} voice`,
+            value: `# ${textChannels} text  |  ${voiceChannels} voice`,
+            inline: false,
+          },
+          {
+            name: "total commands",
+            value: `${totalCommands.rows[0].totalcommands}`,
+            inline: true,
+          },
+          {
+            name: "ban count",
+            value: await (await message.guild.fetchBans()).size,
             inline: true,
           }
-          // {
-          //   name: ,
-          //   value: ,
-          //   inline: true
-          // },
-          // {
-          //   name: ,
-          //   value: ,
-          //   inline: true
-          // },
         )
         .setImage(message.guild.icon)
         .setFooter(
-          `created: ${message.guild.createdAt.toDateString()} | (${new Date(
-            Date.now() - message.guild.createdTimestamp
+          `created: ${message.guild.createdAt.toDateString()} | (${formatDistanceToNow(
+            message.guild.createdTimestamp
           )} ago)`
         )
     );
