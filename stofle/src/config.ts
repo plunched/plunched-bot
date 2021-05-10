4ac8a161f9b9ee1ba42c082d4e3a594df1ed8270
plunched <smoll.pp.exe@gmail.com>
2021-05-10 20:40:57 +0200

    token fix

--- stofle/src/config.ts
+++ stofle/src/config.ts
@@ -1,5 +1,5 @@
 export let token: string =
-  "ODA4MzYwOTk4MDk5NDg0NzIz.YCFa2A.maDtt4N9GVCsWtzD6-xEPNs90d4";
+  "bot toke here";
 export let prefix: string | string[] = ["p.", "."];
 export let owners: string | string[] = ["531953738491887627"];
 export let colors = {
